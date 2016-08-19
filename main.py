6807b4e629cb1c3f0d1f7f516c0f25b585b9ac1d
David Yoo <daooyoo@gmail.com>
2016-08-19 10:21:24 -0500

    added html for signup form

--- main.py
+++ main.py
@@ -16,10 +16,70 @@
 #
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>SignUp</title>
+        <style>
+            .error {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+    </body>
+</html>
+"""
+
+class SignUp(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        signup_form = """
+        <h1>Signup</h1>
+            <form method="post">
+                <table>
+                    <tr>
+                        <td><label for="username">Username</label></td>
+                        <td>
+                            <input name="username" type="text" value ="" required>
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label for="password">Password</label></td>
+                        <td>
+                            <input name="password" type="password" required>
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label for="verify">Verify Password</label</td>
+                        <td>
+                            <input name="verify" type="password" required>
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label for="email">Email (optional)</label></td>
+                        <td>
+                            <input name="email" type="email" value="">
+                            <span class="error"></span>
+                        </td>
+                    </tr>
+                </table>
+                <input type="submit">
+            </form>
+        """
+
+        response = page_header + signup_form + page_footer
+        self.response.write(response)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/signup', SignUp)
 ], debug=True)
