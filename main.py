e1110e3d2e727e1954be7a3d32d0073a8b799c3d
David Yoo <daooyoo@gmail.com>
2016-08-19 11:34:13 -0500

    added welcome screen and error handling

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 import webapp2
+import re
 
 # html boilerplate for the top of every page
 page_header = """
@@ -37,49 +38,104 @@ page_footer = """
 </html>
 """
 
+signup_form = """
+<h1>Signup</h1>
+    <form method="post">
+        <table>
+            <tr>
+                <td><label for="username">Username</label></td>
+                <td>
+                    <input name="username" type="text" value ="%(username)s" required>
+                    <span class="error">%(username_error)s</span>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="password">Password</label></td>
+                <td>
+                    <input name="password" type="password" required>
+                    <span class="error">%(password_error)s</span>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="verify">Verify Password</label</td>
+                <td>
+                    <input name="verify" type="password" required>
+                    <span class="error">%(verify_error)s</span>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="email">Email (optional)</label></td>
+                <td>
+                    <input name="email" type="email" value="%(email)s">
+                    <span class="error">%(email_error)s</span>
+                </td>
+            </tr>
+        </table>
+        <input type="submit">
+    </form>
+"""
+
+welcome_form = """
+    <h1>Welcome, %s!</h1>
+"""
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
 class SignUp(webapp2.RequestHandler):
+    def write_signup_form(self, username="", username_error="", password_error="", verify_error="", email="", email_error=""):
+        self.response.out.write(page_header + signup_form % {"username": username, "username_error": username_error, "password_error": password_error, "verify_error": verify_error, "email": email, "email_error": email_error} + page_footer)
+
     def get(self):
-        signup_form = """
-        <h1>Signup</h1>
-            <form method="post">
-                <table>
-                    <tr>
-                        <td><label for="username">Username</label></td>
-                        <td>
-                            <input name="username" type="text" value ="" required>
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="password">Password</label></td>
-                        <td>
-                            <input name="password" type="password" required>
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="verify">Verify Password</label</td>
-                        <td>
-                            <input name="verify" type="password" required>
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="email">Email (optional)</label></td>
-                        <td>
-                            <input name="email" type="email" value="">
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                </table>
-                <input type="submit">
-            </form>
-        """
+        self.write_signup_form()
 
-        response = page_header + signup_form + page_footer
-        self.response.write(response)
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        username_error = ""
+        password = self.request.get('password')
+        password_error = ""
+        verify = self.request.get('verify')
+        verify_error = ""
+        email = self.request.get('email')
+        email_error = ""
 
+        if not valid_username(username):
+            username_error = "Invalid Username"
+            have_error = True
+
+        if not valid_password(password):
+            password_error = "Invalid Password"
+            have_error = True
+
+        if verify != password:
+            verify_error = "Passwords Don't Match"
+            have_error = True
+
+        if not valid_email(email):
+            email_error = "Invalid Email"
+            have_error = True
+
+        if have_error:
+            self.write_signup_form(username, username_error, password_error, verify_error, email, email_error)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        self.response.out.write(welcome_form % username)
 
 app = webapp2.WSGIApplication([
-    ('/signup', SignUp)
+    ('/', SignUp),
+    ('/welcome', Welcome)
 ], debug=True)
